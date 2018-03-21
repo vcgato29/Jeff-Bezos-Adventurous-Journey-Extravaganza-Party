00177e597338f722d096c834eb35681d54bd03f6
macktvz <macktvz@gmail.com>
2018-03-21 13:49:23 -0400

    sry

--- game.java
+++ game.java
@@ -14,11 +14,28 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.event.*;
 
-
 public class Game {
-   public Game() {
-      JFrame frame = new JFrame("Jeff Bezos' adventure time");
-      frame.setSize(1280,960);
-      
-      
    
+   Drawing draw = new Drawing();
+   public Game() { 
+      JFrame frame = new JFrame("Jeff Bezos' Adventorous Journey Extravagant Party");
+      frame.setSize(1280,960);
+      frame.add(draw);
+      frame.setVisible(true);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      }
+      
+   public static void main(String[] args) {
+    new Game();
+  }
+      
+   public class Drawing extends JComponent {
+      public void paint(Graphics g) {
+         g.setColor(Color.BLUE);
+         g.drawLine(0,400,1280,400);
+         g.drawRect(640,370,30,30);
+         }
+ }
+ 
+ 
+}
